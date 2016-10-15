1bad6fc4626aa7b1b1e04164ab3b2c6ef1dbca07
Silver-T <tony.silvestre@gmail.com>
2016-10-15 21:08:37 +1100

    Implementation using 3 Matrices
    The 3 Matrices are cleaner and easier to work with than a 4D array

--- Part 4.py
+++ Part 4.py
@@ -4,7 +4,7 @@ Created: 12/10/16
 Author: Anthony Silvestre
 Purpose: 
 """
-
+import numpy as np
 '''
 def opt_alignment(seq1, seq2, sub_mat):
 '''    
@@ -13,47 +13,95 @@ def edit_distance(seq1, seq2):
     '''
     Computes the edit distance betweeen the two input sequences iteratively
     '''
-    #edit = [x[:] for x in [[0]*(len(seq2))]*(len(seq1))]
-    edit = [[[[['-'] for i in range(0,5)] for i in range(3)] for i in range(0,len(seq2))] for i in range (0, len(seq1))]
+    edit = [x[:] for x in [[0]*(len(seq2)+1)]*(len(seq1)+1)]
+    edit_type = [x[:] for x in [[0]*(len(seq2)+1)]*(len(seq1)+1)]
+    edit_path = [x[:] for x in [[0]*(len(seq2)+1)]*(len(seq1)+1)]
+    print()
     # sequence 1 is along the x axis
     # sequence 2 is along the y axis
-    edit = np.array(edit)    
-    edit[0][0][0][0] = 0
-    edit[0][0][0][1] = 'M'
-    edit[0][0][0][2] = '-'
-    edit[0][0][0][3] = '-'
-    edit[0][0][0][4] = '-'
+  
+    #edit = np.array(edit)    
+    edit[0][0] = 0
+    edit_type[0][0] = 'M'
+    edit_path[0][0] = '-'
 
-    for i in range(1, len(seq1)):
-        edit[i][0][0][0] = i                    # current value
-        edit[i][0][0][1] = 'I'                  # current type    
-        edit[i][0][0][2] = edit[i-1][0][0][1]   # previous type
-        edit[i][0][0][3] = i-1                  # previous x coord
-        edit[i][0][0][4] = 0                    # previous y coord
+    for i in range(1, len(seq1)+1):
+        edit[i][0]= edit[i-1][0] + g_e
+        edit_type[i][0] = 'I'
+        edit_path[i][0] = 'l'
     
-    for j in range(1, len(seq2)):
-        edit[0][j][0][0] = j
-        edit[0][j][0][1] = 'I'
-        edit[0][j][0][2] = edit[0][j-1][0][1]
-        edit[0][j][0][3] = 0
-        edit[0][j][0][4] = j-1
-
-    for i in range(0, len(seq1)):
-        print(edit[i][0][0][0])
-    '''
-    for i in range(1, len(seq1)):   # iterates over the rows of edit
-        for j in range(1, len(seq2)):   # iterates over the columns of edit
+    for j in range(1, len(seq2)+1):
+        edit[0][j]= edit[0][j-1] + g_e
+        edit_type[0][j] = 'D'
+        edit_path[0][j] = 'u'
+        
+    for i in range(1, len(seq1)+1):       # iterates over the rows of edit
+        for j in range(1, len(seq2)+1):   # iterates over the columns of edit
             if seq1[i-1] == seq2[j-1]:
-                edit[i][j] = edit[i - 1][j - 1]         
+                edit[i][j] = edit[i - 1][j - 1]
+                edit_type[i][j] = 'M'
+                edit_path[i][j] = 'u_l'
                 # If the two letters are a match, the edit cost is zero
             else:
-                edit[i][j] = min(edit[i - 1][j - 1]+1, (edit[i - 1][j]) + 1, (edit[i][j - 1]) + 1)
-                # need to find min of insert, delete or sub equation
+                # Find min surrounding value
+                # Find type of that value
+                # enter that function
+                min_val = min(edit[i-1][j-1], edit[i-1][j], edit[i][j-1])                
+                if edit[i-1][j-1] == min_val:
+                    # Match/Substition
+                    cost = Match_cost(seq1[i-1], seq2[j-1], edit_type[i-1][j-1])
+                    edit[i][j] = edit[i-1][j-1] + cost
+                    edit_type[i][j] = 'M'
+                    edit_path[i][j] = 'u_l'
+                elif edit[i-1][j] == min_val:
+                    # Insertion
+                    cost = Insert_cost(seq1[i-1], seq2[j-1], edit_type[i-1][j])
+                    edit[i][j] = edit[i-1][j] + cost
+                    edit_type[i][j] = 'I'
+                    edit_path[i][j] = 'l'
+                elif edit[i][j-1] == min_val:
+                    # Deletion
+                    cost = Delete_cost(seq1[i-1], seq2[j-1], edit_type[i][j-1])
+                    edit[i][j] = edit[i][j-1] + cost
+                    edit_type[i][j] = 'D'
+                    edit_path[i][j] = 'u'
+               
+    print(np.array(edit))    
     return edit[len(seq1)-1][len(seq2)-1]
-    '''
+   
+def Match_cost(letter1, letter2, prev_type):
+    row = sub_dict[letter1]+1
+    col = sub_dict[letter2]+1
     
-import numpy as np
+    if prev_type == 'M':
+        return int(submat[row][col])
+    elif prev_type == 'D' or prev_type == 'I':
+        return g_0
 
+def Delete_cost(letter1, letter2, prev_type):
+    row = sub_dict[letter1]+1
+    col = sub_dict[letter2]+1
+    
+    if prev_type == 'M':
+        return int(submat[row][col])
+    elif prev_type == 'D':
+        return g_e
+    elif prev_type == 'I':
+        return g_0
+
+def Insert_cost(letter1, letter2, prev_type):
+    row = sub_dict[letter1]+1 
+    col = sub_dict[letter2]+1
+    
+    if prev_type == 'M':
+        return int(submat[row][col])
+    elif prev_type == 'D':
+        return g_0
+    elif prev_type == 'I':
+        return g_e  
+    
+    
+'''
 a = open('seq1', 'r')
 a_seq = a.read()
 a.close()
@@ -62,5 +110,25 @@ b_seq = b.read()
 b.close()
 print(a_seq)
 print(b_seq)
+'''
+f = open('submat', 'r')
+submat = [line.split() for line in f]
+f.close()
 
-print(edit_distance(a_seq, b_seq))
+# Constructs a dictionary to easily find the index of a letter
+sub_dict = {}
+count = 0
+for letter in submat[0]:
+    sub_dict[letter] = count
+    count += 1
+
+# define global variable: g_0, gap opening penalty
+g_0 = 10                    # so it's not favourable
+
+# define global variable: g_e, gap extension penalty
+g_e = 5                     # more favourable then opening a new gap
+
+'''
+edit_distance(b_seq, a_seq)
+'''
+edit_distance('ATA', 'AGTA')
